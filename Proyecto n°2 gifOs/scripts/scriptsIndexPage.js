cfb6e815271def69c293ad74e9f53310b4dae86e
Marto <martinbouzada@outlook.com>
2020-06-27 01:30:06 -0300

    1.2
    Se guarda el resultado de las ultimas busquedas

--- Proyecto n°2 gifOs/scripts/scriptsIndexPage.js
+++ Proyecto n°2 gifOs/scripts/scriptsIndexPage.js
@@ -1,165 +1,195 @@
 const $apikey = 'UOL3lcP2fQqwUqxTXiL4RyuBBfyAIQE2';
 
 //Sugerencias + seleccion random
-const $suggestionsArray = ["Barney Stinson", "Ferrari", "Sony", "Samsung", "Apple", "Greys Anatomy", "F1", "Play Station", "Futbol"];
+const $suggestionsArray = ['Barney Stinson', 'Ferrari', 'Sony', 'Metal Gear', 'Apple', 'Greys Anatomy', 'F1', 'Play Station', 'Futbol'];
 const $suggestionsRandomPick = $suggestionsArray[Math.floor(Math.random() * $suggestionsArray.length)];
 
 //
 let tau = '1';
 
 function reloadPage() {
-  location.reload();
+    location.reload();
 }
 
 //Funcion para generar el localstorage de las busqueadas realizadas
 function savedSearchesLS() {
-  return localStorage.getItem("searches") == undefined
-      ? localStorage.setItem("searches", "[]")
-      : console.log('Local Storage ya generado');
+    return localStorage.getItem('searches') == undefined ?
+        localStorage.setItem('searches', '[]') :
+        console.log('Local Storage ya generado');
 }
 
 //Menú desplegable donde están los 2 themes 
 function themeMenu() {
-  var div = document.getElementsByClassName("theme-menu")[0];
-  if (div.style.display == "flex") {
-    div.style.display = "none";
-  } else {
-    div.style.display = "flex";
-  }
+    var div = document.getElementsByClassName('theme-menu')[0];
+    if (div.style.display == 'flex') {
+        div.style.display = 'none';
+    } else {
+        div.style.display = 'flex';
+    }
 }
 
 //Cambio de theme
 function changetheme(cssfile) {
-  document.getElementById('csstheme').setAttribute('href', cssfile);
-  localStorage.setItem("theme", cssfile);
+    document.getElementById('csstheme').setAttribute('href', cssfile);
+    localStorage.setItem('theme', cssfile);
 }
 
 //Cambio de logo
 function changelogo(logo) {
-  document.getElementById('logo').setAttribute('src', logo);
+    document.getElementById('logo').setAttribute('src', logo);
 }
 
 //Cambio de dropdown
 function changedropdown(dropdown) {
-  document.getElementById('dropdown').setAttribute('src', dropdown);
+    document.getElementById('dropdown').setAttribute('src', dropdown);
 }
 
 //Función para realizar la búsqueda de los GIF
 function getSearchResults(search) {
 
-  if (search == undefined) {
-    var search = document.getElementById('search-textfield').value;
-  }
+    if (search == undefined) {
+        var search = document.getElementById('search-textfield').value;
+    }
+    const found =
+        fetch('http://api.giphy.com/v1/gifs/search?q=' + search +
+            '&api_key=' + $apikey)
+        .then((response) => {
 
-  const found =
-    fetch('http://api.giphy.com/v1/gifs/search?q=' + search +
-      '&api_key=' + $apikey)
-      .then((response) => {
-        return response.json()
-      }).then(data => {
+            return response.json()
+        }).then(data => {
+            for (i = 0; i <= 9; i++) {
+                var $trendingGif = data.data;
+                var divv = document.getElementById('trending-gif' + i);
+                let prueba2 = document.getElementById('trending-gif-bar' + i);
+                gif = $trendingGif[i].images.original.url;
+                let title1 = $trendingGif[i].title.slice(0, 35);
+                divv.style.backgroundImage = 'url(' + gif + ')';
+                prueba2.innerHTML = '#' + title1;
+            }
+        })
+        .catch((error) => {
+            return error
+        })
+    tau = '1';
+    searchBtn(trending());
 
-        for (i = 0; i <= 9; i++) {
-          var $trendingGif = data.data;
-          var divv = document.getElementById("trending-gif" + i);
-          let prueba2 = document.getElementById("trending-gif-bar" + i);
-          gif = $trendingGif[i].images.original.url;
-          let title1 = $trendingGif[i].title.slice(0, 35);
-          divv.style.backgroundImage = "url(" + gif + ")";
-          prueba2.innerHTML = "#" + title1;
-        }
-      })
-      .catch((error) => {
-        return error
-      })
 
-  tau = '1';
-  searchBtn(trending());
+    let searches = JSON.parse(localStorage.getItem('searches'));
+    searches.push(search);
+    localStorage.setItem('searches', JSON.stringify(searches));
+    savedSearches();
 }
 
 //Función para las Tendencias.
 let $trendinglimit = '10';
 
 function trending() {
-  const found =
-    fetch('http://api.giphy.com/v1/gifs/trending?' + '&api_key=' + $apikey + '&limit=' + $trendinglimit)
-      .then((response) => {
-        return response.json()
-      }).then(data => {
-        for (i = 0; i <= 9; i++) {
-          var $trendingGif = data.data;
-          var divv = document.getElementById("trending-gif" + i);
-          let prueba2 = document.getElementById("trending-gif-bar" + i);
-          gif = $trendingGif[i].images.original.url;
-          let title1 = $trendingGif[i].title.slice(0, 35);
-          divv.style.backgroundImage = "url(" + gif + ")";
-          prueba2.innerHTML = "#" + title1;
-        }
-      })
-      .catch((error) => {
-        return error
-      })
-  return found
+    const found =
+        fetch('http://api.giphy.com/v1/gifs/trending?' + '&api_key=' + $apikey + '&limit=' + $trendinglimit)
+        .then((response) => {
+            return response.json()
+        }).then(data => {
+            for (i = 0; i <= 9; i++) {
+                var $trendingGif = data.data;
+                var divv = document.getElementById('trending-gif' + i);
+                let prueba2 = document.getElementById('trending-gif-bar' + i);
+                gif = $trendingGif[i].images.original.url;
+                let title1 = $trendingGif[i].title.slice(0, 35);
+                divv.style.backgroundImage = 'url(' + gif + ')';
+                prueba2.innerHTML = '#' + title1;
+            }
+        })
+        .catch((error) => {
+            return error
+        })
+    return found
 
 }
 
 //Función para los gif sugeridos
 function suggestions() {
-  fetch('http://api.giphy.com/v1/gifs/search?' + 'api_key=' + $apikey + '&q=' + $suggestionsRandomPick + '&limit=4')
-    .then((response) => {
-      return response.json()
-    }).then(data => {
-      for (i = 0; i <= 3; i++) {
-        var $suggestionGifUrl = data.data;
-        var suggestionGif = document.getElementById("suggestions-gif" + i);
-        let hashtagSuggestion = document.getElementById("#" + i);
-        gif2 = $suggestionGifUrl[i].images.original.url;
-        let title2 = $suggestionGifUrl[i].title.slice(0, 35);
-        suggestionGif.style.backgroundImage = "url(" + gif2 + ")";
-        hashtagSuggestion.innerHTML = "#" + title2;
-      }
-    })
-    .catch((error) => {
-      alert("Error al obtener las sugerencias, recargar la página");
-      return error
-    })
+    fetch('http://api.giphy.com/v1/gifs/search?' + 'api_key=' + $apikey + '&q=' + $suggestionsRandomPick + '&limit=4')
+        .then((response) => {
+            return response.json()
+        }).then(data => {
+            for (i = 0; i <= 3; i++) {
+                var $suggestionGifUrl = data.data;
+                var suggestionGif = document.getElementById('suggestions-gif' + i);
+                let hashtagSuggestion = document.getElementById('#' + i);
+                gif2 = $suggestionGifUrl[i].images.original.url;
+                let title2 = $suggestionGifUrl[i].title.slice(0, 35);
+                suggestionGif.style.backgroundImage = 'url(' + gif2 + ')';
+                hashtagSuggestion.innerHTML = '#' + title2;
+            }
+        })
+        .catch((error) => {
+            alert('Error al obtener las sugerencias, recargar la página');
+            return error
+        })
 }
 
-//Funcion para mostrar las ultimas 5 busquedas
+//Funciones donde se obtienen las busquedas guardadas en el ls, y se muestran en forma de btns
+function savedSearches() {
+    let $savedSearches = JSON.parse(localStorage.getItem('searches'));
+    let $sectionButtons = document.getElementById('saved-searches');
+    $count = $savedSearches.length - 1;
 
-// function savedSearches() {
-//   let $savedSearches = JSON.parse(localStorage.getItem(""));
-//   if ($savedSearches != undefined && $savedSearches.length > 0) {
-//       for (let i = 0; i < $savedSearches.length; i++) {
-//           let grid = document.getElementById("grid" + i);
-//           fetch("https://api.giphy.com/v1/gifs/" + gifIds[i] + "?api_key=" + apiKey)
-//               .then(response => {
-//                   return response.json();
-//               })
-//               .then(data => {
-//                   var gif = data.data.images.original.url;
-//                   grid.style.backgroundImage = "url(" + gif + ")";
-//               });
-//       }
-//   }
-// }
+    $count <= 1 ?
+        $sectionButtons.style.display = 'inline' :
+        console.log('Ya avanzo de 0');
+
+    $sectionButtons.style.display = 'inline';
+    let $btns = document.createElement('button');
+    $sectionButtons.appendChild($btns);
+    $btns.setAttribute('class', 'saved-searches-btn');
+    $btns.setAttribute('id', 'saved-searches-btn' + $count);
+    let $busquedas = document.getElementById('saved-searches-btn' + $count);
+    $busquedas.innerHTML = '#' + $savedSearches[$count]
+    $busquedas.style.display = 'inline';
+}
+
+function createBtns() {
+    let $savedSearches = JSON.parse(localStorage.getItem('searches'));
+    let $sectionButtons = document.getElementById('saved-searches');
+    $count = $savedSearches.length - 1;
+
+    if ($savedSearches != undefined && $savedSearches.length > 0) {
+        for (let i = 0; i < $savedSearches.length; i++) {
+
+            i == '0' ?
+                $sectionButtons.style.display = 'inline' :
+                console.log('Ya avanzo de 0 y esta llegando a 4');
+
+            $sectionButtons.style.display = 'inline';
+            let $btns = document.createElement('button');
+            $sectionButtons.appendChild($btns);
+            $btns.setAttribute('class', 'saved-searches-btn');
+            $btns.setAttribute('id', 'saved-searches-btn' + i);
+            let $busquedas = document.getElementById('saved-searches-btn' + i);
+            $busquedas.innerHTML = '#' + $savedSearches[i]
+            $busquedas.style.display = 'inline';
+
+        }
+    }
+
+}
 
 //Ejecuciones de funciones
 savedSearchesLS();
+createBtns();
 trending();
 suggestions();
 
-
 // Funcion de busqueda de gifs
 function searchBtn() {
-  var suggestionsSection = document.getElementsByClassName("suggestions")[0];
-  let prueba1 = document.getElementById('suggestions-text');
+    var suggestionsSection = document.getElementsByClassName('suggestions')[0];
+    let prueba1 = document.getElementById('suggestions-text');
 
-  if (tau == '1') {
-    suggestionsSection.style.display = "none";
-    prueba1.innerHTML = "Tu busqueda en gifOS";
+    if (tau == '1') {
+        suggestionsSection.style.display = 'none';
+        prueba1.innerHTML = 'Tu busqueda en gifOS'; //Mejorar la frase y agregar la frase buscada.
 
-  } else {
-    alert('La busqueda no se pudo realizar, reinicie la pagina')
-  }
+    } else {
+        alert('La busqueda no se pudo realizar, reinicie la pagina')
+    }
 }
-
--- Proyecto n°2 gifOs/scripts/scriptsRecordPage.js
+++ Proyecto n°2 gifOs/scripts/scriptsRecordPage.js
@@ -2,15 +2,15 @@ const apiKey = 'UOL3lcP2fQqwUqxTXiL4RyuBBfyAIQE2';
 
 //Funciones donde se generan los local storage
 function idsLocalStorage() {
-    return localStorage.getItem("gifIds") == undefined
-        ? localStorage.setItem("gifIds", "[]")
-        : console.log('Local Storage ya generado');
+    return localStorage.getItem('gifIds') == undefined ?
+        localStorage.setItem('gifIds', '[]') :
+        console.log('Local Storage ya generado');
 }
 
 function lastGif() {
-    return localStorage.getItem("lastGif") == undefined
-        ? localStorage.setItem("lastGif", "")
-        : console.log('Local Storage ya generado');
+    return localStorage.getItem('lastGif') == undefined ?
+        localStorage.setItem('lastGif', '') :
+        console.log('Local Storage ya generado');
 }
 
 idsLocalStorage();
@@ -23,38 +23,37 @@ function reloadPage() {
 
 //Funciones para realizar la grabacion de los gifs
 function getStreamAndRecord() {
-    var video = document.getElementById("video");
-    let record = document.getElementById("record");
-    let recordHeader = document.getElementById("record-header");
-    let article = document.getElementById("article");
-    let footer = document.getElementById("footer");
-    let btn9 = document.getElementById("btn9");
-    let btn10 = document.getElementById("btn10");
-    var btn11 = document.getElementById("btn11");
-    var btn12 = document.getElementById("btn12");
-    let videoContainer = document.getElementById("video-container");
-    var preview = document.getElementById("preview");
+    var video = document.getElementById('video');
+    let record = document.getElementById('record');
+    let recordHeader = document.getElementById('record-header');
+    let article = document.getElementById('article');
+    let footer = document.getElementById('footer');
+    let btn9 = document.getElementById('btn9');
+    let btn10 = document.getElementById('btn10');
+    var btn11 = document.getElementById('btn11');
+    var btn12 = document.getElementById('btn12');
+    let videoContainer = document.getElementById('video-container');
+    var preview = document.getElementById('preview');
     navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-            width: { min: 640 }
-        }
-    }
-    )
-        .then(function (stream) {
-            record.style.width = "667px";
+            audio: false,
+            video: {
+                width: { min: 640 }
+            }
+        })
+        .then(function(stream) {
+            record.style.width = '667px';
             record.style.height = '586px';
             recordHeader.style.marginLeft = '4.5px';
-            article.style.display = "none";
-            footer.style.marginTop = "16px";
-            footer.style.marginRight = "14px";
-            footer.style.marginBottom = "0px";
-            btn9.style.display = "none";
-            btn10.style.display = "none";
-            videoContainer.style.display = "block";
-            btn11.style.display = "inline";
-            btn12.style.display = "inline";
-            preview.style.display = "none";
+            article.style.display = 'none';
+            footer.style.marginTop = '16px';
+            footer.style.marginRight = '14px';
+            footer.style.marginBottom = '0px';
+            btn9.style.display = 'none';
+            btn10.style.display = 'none';
+            videoContainer.style.display = 'block';
+            btn11.style.display = 'inline';
+            btn12.style.display = 'inline';
+            preview.style.display = 'none';
 
             video.srcObject = stream;
             video.play()
@@ -62,19 +61,19 @@ function getStreamAndRecord() {
 };
 
 function grabarGif() {
-    let btn13 = document.getElementById("btn13");
-    let btn14 = document.getElementById("btn14");
-    let btn17 = document.getElementById("btn17");
-    let btn18 = document.getElementById("btn18");
+    let btn13 = document.getElementById('btn13');
+    let btn14 = document.getElementById('btn14');
+    let btn17 = document.getElementById('btn17');
+    let btn18 = document.getElementById('btn18');
 
     navigator.mediaDevices.getUserMedia({
-        video: true
-    })
-        .then(function (stream) {
-            btn11.style.display = "none";
-            btn12.style.display = "none";
-            btn13.style.display = "inline";
-            btn14.style.display = "inline";
+            video: true
+        })
+        .then(function(stream) {
+            btn11.style.display = 'none';
+            btn12.style.display = 'none';
+            btn13.style.display = 'inline';
+            btn14.style.display = 'inline';
             var recorder = RecordRTC(stream, {
                 type: 'gif',
                 frameRate: 60,
@@ -85,46 +84,45 @@ function grabarGif() {
             recorder.startRecording();
 
             function detenerGrabacion() {
-                recorder.stopRecording(function () {
+                recorder.stopRecording(function() {
                     let blob = recorder.getBlob();
                     let gifPreview = URL.createObjectURL(blob);
 
-                    video.style.display = "none";
+                    video.style.display = 'none';
                     preview.src = gifPreview;
-                    preview.style.display = "inline";
+                    preview.style.display = 'inline';
                 });
-                btn13.style.display = "none";
-                btn14.style.display = "none";
-                btn17.style.display = "inline";
-                btn18.style.display = "inline";
+                btn13.style.display = 'none';
+                btn14.style.display = 'none';
+                btn17.style.display = 'inline';
+                btn18.style.display = 'inline';
 
                 function subirGif() {
-                    let gifIds = JSON.parse(localStorage.getItem("gifIds"));
-                    var uploadDiv = document.getElementById("uploadDiv");
-                    var btn19 = document.getElementById("btn19");
-                    btn17.style.display = "none";
-                    btn18.style.display = "none";
-                    btn19.style.display = "inline";
-                    uploadDiv.style.display = "flex";
-                    preview.style.display = "none";
+                    let gifIds = JSON.parse(localStorage.getItem('gifIds'));
+                    var uploadDiv = document.getElementById('uploadDiv');
+                    var btn19 = document.getElementById('btn19');
+                    btn17.style.display = 'none';
+                    btn18.style.display = 'none';
+                    btn19.style.display = 'inline';
+                    uploadDiv.style.display = 'flex';
+                    preview.style.display = 'none';
 
                     let form = new FormData();
-                    form.append("file", recorder.getBlob(), "myGif.gif");
+                    form.append('file', recorder.getBlob(), 'myGif.gif');
 
                     const upload = fetch(
-                        "https://upload.giphy.com/v1/gifs" + "?api_key=" + apiKey,
-                        {
-                            method: "POST",
-                            body: form
-                        }
-                    )
+                            'https://upload.giphy.com/v1/gifs' + '?api_key=' + apiKey, {
+                                method: 'POST',
+                                body: form
+                            }
+                        )
                         .then(response => {
                             return response.json();
                         })
                         .then(data => {
                             var gifId = data.data.id // ID del gif que se acaba de subir
                             setTimeout(previewGif(), 5000);
-                            const searchId = fetch("https://api.giphy.com/v1/gifs/" + gifId + "?api_key=" + apiKey)
+                            const searchId = fetch('https://api.giphy.com/v1/gifs/' + gifId + '?api_key=' + apiKey)
                                 .then(response => {
                                     return response.json();
                                 })
@@ -133,52 +131,53 @@ function grabarGif() {
                                     let $previewGif = data.data;
                                     let $urlGif = data.data.bitly_gif_url;
                                     let $urlPreviewGif = $previewGif.images.fixed_height_downsampled.url;
-                                    let $preview = document.getElementById("Giifpreview");
-                                    $preview.style.backgroundImage = "url(" + $urlPreviewGif + ")";
-                                    $preview.style.backgroundSize = "cover";
+                                    let $preview = document.getElementById('Giifpreview');
+                                    $preview.style.backgroundImage = 'url(' + $urlPreviewGif + ')';
+                                    $preview.style.backgroundSize = 'cover';
                                     gifIds.push(gifId);
-                                    localStorage.setItem("gifIds", JSON.stringify(gifIds));
-                                    localStorage.setItem("lastGif", JSON.stringify($urlGif));
+                                    localStorage.setItem('gifIds', JSON.stringify(gifIds));
+                                    localStorage.setItem('lastGif', JSON.stringify($urlGif));
                                 })
                             return searchId;
                         })
-                        .catch(function (error) {
+                        .catch(function(error) {
                             alert('El upload del gif falló - Error: ' + error)
                         });
                     return upload
                 }
-                btn18.addEventListener("click", subirGif);
+                btn18.addEventListener('click', subirGif);
             }
-            btn14.addEventListener("click", detenerGrabacion);
+            btn14.addEventListener('click', detenerGrabacion);
         })
 
     function previewGif() {
-        let cuadroPrincipal = document.getElementById("container-record-gif");
-        let nuevoCuadro = document.getElementById("success");
-        cuadroPrincipal.style.display = "none";
-        nuevoCuadro.style.display = "inline";
+        let cuadroPrincipal = document.getElementById('container-record-gif');
+        let nuevoCuadro = document.getElementById('success');
+        cuadroPrincipal.style.display = 'none';
+        nuevoCuadro.style.display = 'inline';
     }
 };
 
 //Copiar y descargar el gif (Se abre en una nueva ventana)
 function copyGif() {
-    let texto = document.createElement("textarea");
-    let $lastGif = localStorage.getItem("lastGif");
+    let texto = document.createElement('textarea');
+    let $lastGif = localStorage.getItem('lastGif');
     texto.value = $lastGif
     document.body.appendChild(texto);
     texto.select();
-    document.execCommand("copy");
+    document.execCommand('copy');
     document.body.removeChild(texto);
-    alert("El gif se copió al portapapeles");
+    alert('El gif se copió al portapapeles');
 }
+
 function downloadGif() {
-    let $lastGif = localStorage.getItem("lastGif");
-    window.open($lastGif, "_blank");
+    let $lastGif = localStorage.getItem('lastGif');
+    window.open($lastGif, '_blank');
 }
 
 function btnCancelar() {
-    let hide = document.getElementById("container-record-gif");
-    hide.style.display = "none";
+    let hide = document.getElementById('container-record-gif');
+    hide.style.display = 'none';
 }
 
 function btnListo() {
@@ -186,17 +185,17 @@ function btnListo() {
 }
 
 function localStorageGifs() {
-    let gifIds = JSON.parse(localStorage.getItem("gifIds"));
+    let gifIds = JSON.parse(localStorage.getItem('gifIds'));
     if (gifIds != undefined && gifIds.length > 0) {
         for (let i = 0; i < gifIds.length; i++) {
-            let grid = document.getElementById("grid" + i);
-            fetch("https://api.giphy.com/v1/gifs/" + gifIds[i] + "?api_key=" + apiKey)
+            let grid = document.getElementById('grid' + i);
+            fetch('https://api.giphy.com/v1/gifs/' + gifIds[i] + '?api_key=' + apiKey)
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
                     var gif = data.data.images.original.url;
-                    grid.style.backgroundImage = "url(" + gif + ")";
+                    grid.style.backgroundImage = 'url(' + gif + ')';
                 });
         }
     }
